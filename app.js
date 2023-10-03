45c01953bd04353f54495f26d37c22806ce35ee4
Esra Kazi <74008194+EsraKazi@users.noreply.github.com>
2023-10-03 15:47:05 +0300

    Trying Socket io

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
 
+const socketIO = require('socket.io');
+
 const authRouter = require('./routes/authRoutes');
 const reservationRouter = require('./routes/reservationRoutes');
 
@@ -23,5 +25,27 @@ require('./middleware/setInterval');
 
 mongoose
   .connect(`${process.env.DB_CONNECT}`)
-  .then((result) => app.listen(process.env.PORT))
-  .catch(err => console.error(err));
+  .then((result) => {
+    // Express sunucusunu Socket.io ile başlatın
+    const server = require('http').createServer(app);
+    const io = socketIO(server);
+
+    // Socket.io ile ilgili işlemleri burada gerçekleştirin
+    io.on('connection', (socket) => {
+      console.log('Kullanıcı bağlandı.');
+
+      socket.on('message', (data) => {
+        console.log('Gelen mesaj:', data);
+        // Gelen mesajı işleme kodunu burada ekleyin
+      });
+
+      socket.on('disconnect', () => {
+        console.log('Kullanıcı bağlantıyı kesti.');
+      });
+    });
+
+    server.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
